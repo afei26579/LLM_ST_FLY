6a5c5b3cddb4776a4a6a9e6295b5cda921059ff0
afei26579 <631008153@qq.com>
2025-08-08 16:27:36 +0800

    修复icorn 日志路径

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -3,5 +3,5 @@ bind = 'unix:/var/run/gunicorn/llm_st_fly.sock'
 workers = 3
 worker_class = 'sync'
 timeout = 120
-access_logfile = '/var/log//gunicorn/gunicorn_access.log'
-error_logfile = '/var/log//gunicorn/gunicorn_error.log'
+access_logfile = '/var/log/gunicorn/gunicorn_access.log'
+error_logfile = '/var/log/gunicorn/gunicorn_error.log'
